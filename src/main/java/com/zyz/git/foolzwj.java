0cc6f18b011d105748e39889c066a0afc447b8fd
zyz <zyz.@qq.com>
2022-08-08 21:09:06 +0800

    first

---
+++ src/main/java/com/zyz/git/foolzwj.java
@@ -0,0 +1,14 @@
+package com.zyz.git;
+
+/**
+ * @author zhangyizhang
+ * @description：
+ * @create 2022-08-08 21:04
+ */
+
+
+public class foolzwj {
+    public static void main(String[] args) {
+        System.out.println("朱文举，是一个傻瓜!!!!!");
+    }
+}
